7d41583e6a2fff4f5019620e4ad9b0df94c33153
Str4ch <dog.fek54@gmail.com>
2024-09-23 07:47:45 +0200

    second project

---
+++ first prj/main.js
@@ -0,0 +1,7 @@
+const toggle_btn = document.querySelector(".toggle-btn");
+
+toggle_btn.addEventListener("click", () => {
+    // select the hoprizontal navBar
+    // toggle class named 'show' on each click
+	document.querySelector(".navigationBar").classList.toggle("show")
+})
